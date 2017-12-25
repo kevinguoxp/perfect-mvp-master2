a388904a08776d8422c6e64867ffe16af0cb32e8
Thomasguoxp <Thomasguoxp@users.noreply.github.com>
2017-12-25 17:14:52 +0800

    init

--- app/src/main/java/com/ljq/mvp/demo3/RequestMode3.java
+++ app/src/main/java/com/ljq/mvp/demo3/RequestMode3.java
@@ -9,7 +9,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-/**
+/**123----------------------------
  * @author 刘镓旗
  * @date 2017/11/16
  * @description M层
